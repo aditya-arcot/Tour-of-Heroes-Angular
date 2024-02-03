2699caf9a59778d4109408a65dd8413f7d8c7f83
aditya-arcot <arcot.aditya@utexas.edu>
2024-02-03 17:47:53 -0600

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'Tour-of-Heroes-Angular';
+    title = 'Tour of Heroes';
 }
