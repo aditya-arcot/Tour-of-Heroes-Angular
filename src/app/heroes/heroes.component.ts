a62bf2b4ea4ab8da777211ed4c57926f836d0ee3
aditya-arcot <arcot.aditya@utexas.edu>
2024-02-05 14:39:55 -0600

    heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
     selector: 'app-heroes',
@@ -11,8 +12,9 @@ import { FormsModule } from '@angular/forms';
     styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
+    heroes = HEROES
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     }
 }
