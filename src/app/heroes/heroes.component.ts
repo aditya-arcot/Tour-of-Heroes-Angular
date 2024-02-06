5f6a2ff7fa2d05e6928b3784b9a4cb802e621449
aditya-arcot <arcot.aditya@utexas.edu>
2024-02-06 00:33:40 -0600

    add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
     selector: 'app-heroes',
@@ -13,9 +14,18 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
     styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-    heroes = HEROES
+    heroes: Hero[] = [];
     selectedHero?: Hero;
+    constructor(private heroService: HeroService, private messageService: MessageService) { }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
     onSelect(hero: Hero): void {
+        this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
         this.selectedHero = hero;
     }
 }
