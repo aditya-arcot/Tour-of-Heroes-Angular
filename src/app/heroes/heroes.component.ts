589664c0780ebde0e1402a90424616479213e4ab
aditya-arcot <arcot.aditya@utexas.edu>
2024-02-06 10:15:37 -0600

    create server, operations

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,4 +23,14 @@ export class HeroesComponent {
         this.heroService.getHeroes()
             .subscribe(heroes => this.heroes = heroes);
     }
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(_ => this.getHeroes())
+    }
+    delete(hero: Hero): void {
+        this.heroService.deleteHero(hero.id)
+            .subscribe(_ => this.getHeroes());
+    }
 }
